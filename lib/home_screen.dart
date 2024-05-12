c11f95003e71bf0017cc920892e54f3794a0a068
DevNadimm <nadimchowdhury87@gmail.com>
2024-05-12 12:50:19 +0600

    All works

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,16 +11,15 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _glassNumberTEController =
-      TextEditingController();
+  TextEditingController();
   List<WaterConsume> waterConsumeList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Water Tracker',style: TextStyle(fontWeight: FontWeight.w500),),
+        title: const Text('Water Tracker', style: TextStyle(fontWeight: FontWeight.w500)),
       ),
-
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -31,26 +30,24 @@ class _HomeScreenState extends State<HomeScreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   GestureDetector(
-                    onTap:  _tapButton ,
+                    onTap: _tapButton,
                     child: Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(100),
-                        border: Border.all(width: 10,color: const Color(0xffAD88C6))
+                        border: Border.all(width: 10, color: const Color(0xffAD88C6)),
                       ),
                       child: const Padding(
                         padding: EdgeInsets.all(18.0),
                         child: Column(
                           children: [
-                            Icon(Icons.water_drop_outlined,size: 40,color: Color(0xff7469B6),),
-                            Text('Tap Here',style: TextStyle(color: Color(0xff7469B6),fontWeight: FontWeight.w500),)
+                            Icon(Icons.water_drop_outlined, size: 40, color: Color(0xff7469B6)),
+                            Text('Tap Here', style: TextStyle(color: Color(0xff7469B6), fontWeight: FontWeight.w500)),
                           ],
                         ),
                       ),
                     ),
                   ),
-
-                  const SizedBox(height: 15,),
-
+                  const SizedBox(height: 15),
                   SizedBox(
                     width: 60,
                     child: TextField(
@@ -62,37 +59,30 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             ),
-
-            const SizedBox(height: 15,),
-
+            const SizedBox(height: 15),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const Text('History', style: TextStyle(color: Color(0xff7469B6))),
-                Text('Total Glass ${_getTotalWaterConsumeCount.totalGlass}', style: const TextStyle(color: Color(0xff7469B6)))
+                Text('Total Glass ${_getTotalWaterConsumeCount()}', style: const TextStyle(color: Color(0xff7469B6))),
               ],
             ),
-
             Expanded(
               child: ListView.builder(
-                  itemCount: waterConsumeList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      // title: Text((waterConsumeList[index].date).toString()),
-                      title: Text(
-                          (DateFormat.yMEd().add_jms().format(DateTime.now()))
-                              .toString()),
-                      leading: CircleAvatar(
-                        backgroundColor: const Color(0xff7469B6),
-                        foregroundColor: Colors.white,
-                        child: Text('${index + 1}'),
-                      ),
-                      trailing: Text(
-                          (waterConsumeList[index].waterGlasses).toString()),
-                    );
-                  }),
+                itemCount: waterConsumeList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text((DateFormat.yMEd().add_jms().format(DateTime.now())).toString()),
+                    leading: CircleAvatar(
+                      backgroundColor: const Color(0xff7469B6),
+                      foregroundColor: Colors.white,
+                      child: Text('${index + 1}'),
+                    ),
+                    trailing: Text((waterConsumeList[index].waterGlasses).toString()),
+                  );
+                },
+              ),
             ),
-
           ],
         ),
       ),
@@ -109,8 +99,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _tapButton() {
     int glassCount = int.tryParse(_glassNumberTEController.text) ?? 1;
-    WaterConsume waterConsume =
-    WaterConsume(date: DateTime.now(), waterGlasses: glassCount);
+    WaterConsume waterConsume = WaterConsume(date: DateTime.now(), waterGlasses: glassCount);
     waterConsumeList.add(waterConsume);
     setState(() {});
   }
